cee2c79520a17d0359f8eed858e176846c3508de
Eros-Almeida <140436383+Eros-Almeida@users.noreply.github.com>
2025-02-17 15:53:53 -0300

    teste de exemplo de tipos de variável

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade = 40;
+        idade = 30;
+
+        System.out.println(idade);
     }
 }
